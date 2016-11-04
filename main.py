98dac53077cc5f98995a9f650ee9a70f5aec5c14
miltoneiji <miltontakamura@gmail.com>
2016-11-04 20:52:58 -0200

    final

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
+import os
 import glob
-from OCR import OCR
 
-# Entry point
-def main():
-    # Loading input images
-    inputImgs = glob.glob('input/*.png')
+def findWord(word, files):
+    print("Looking for the word: " + word)
 
-    # Starting OCR
-    ocr = OCR(inputImgs)
-    results = ocr.start()
+    for idx in range(0, len(files)):
+        file = open(files[idx], 'r')
+        
+        text = file.read()
+        text = text.lower()
+        if text.find(word) != -1:
+            print("Image " + files[idx][:-4] + ".jpg contains the word " + word)
+            os.system("open input/" + files[idx][:-4] + ".jpg")
 
-    # Printing
-    print results
+        file.close()
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+
+    # Parte1: Obtendo os textos das imagens
+    # Obtem os textos contidos nas images presentes na pasta input
+    # os textos sao nomeados com o mesmo nome das images mas com extensao .txt
+    imageNames = glob.glob('input/*.jpg')
+    files = []
+    for i in range(0, len(imageNames)):
+        files.append(imageNames[i][6:-4] + ".txt")
+        os.system("tesseract " + imageNames[i] + " " + imageNames[i][6:-4])
+        #os.system("open " + imageNames[i][6:-4] + ".txt")
+    os.system("clear")
+    # Parte2: Busca
+    # Pergunta ao usuario que palavra ele busca
+    while True:
+        print("Enter the word that you are looking for: ")
+        word = raw_input('')
+        findWord(word, files)
